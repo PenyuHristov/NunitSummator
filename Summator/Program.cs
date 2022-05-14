4a805ec9d12f2805b0c6d048a48bcea999f3859b
PenyuHristov <105143885+PenyuHristov@users.noreply.github.com>
2022-05-14 18:39:32 +0300

    Initial_Commit

---
+++ Summator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Summator
+{
+    internal class Program
+    {     
+        
+
+
+        static void Main(string[] args)
+        {
+            int result = Summator.Sum(new int[] { 1, 2, 3, 4, 5, 6 });
+
+            if (result == 21)
+            {
+                Console.WriteLine("Test pass");
+            }
+            else
+            {
+                Console.WriteLine("Test fail");
+            }
+        }
+    }
+}
